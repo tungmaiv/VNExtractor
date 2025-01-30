6db0a3efda4b8915d7bada57269a2cd9fab700ea
tungmaiv <tungmaiv@gmail.com>
2025-01-30 20:19:06 +0700

    Create run.py

---
+++ run.py
@@ -0,0 +1,9 @@
+from VNExtractor import create_app
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(
+        host=app.config['HOST'],
+        port=app.config['PORT'],
+        debug=app.config['DEBUG']
+    )
