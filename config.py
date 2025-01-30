c18d3cffc5d1f34a352ee9093a08940084c27082
tungmaiv <tungmaiv@gmail.com>
2025-01-30 20:16:05 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+from pathlib import Path
+
+class Config:
+    VNCORENLP_PATH = os.getenv('VNCORENLP_PATH', '/app/vncorenlp')
+    DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
+    HOST = os.getenv('HOST', '0.0.0.0')
+    PORT = int(os.getenv('PORT', 5000))
